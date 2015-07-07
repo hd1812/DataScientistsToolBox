0dd0a8f8b2afd12d896c3744fa77a22caae0087c
hd1812 <hao.ding12@imperial.ac.uk>
2015-07-07 23:08:32 +0100

    Week 1

---
+++ Intro.R
@@ -0,0 +1,31 @@
+##Lecture Notes
+
+##Ctr+1/Ctr+2 -- Move cursor to source/ Console
+##args("")    -- Return function arguments
+##?function   -- Function cookbook online
+
+##R Programming Overview
+con <- url("http://www.jhsph.edu", "r")
+x <- readLines(con)
+head(x)
+
+##Getting and cleaning data overview
+##read data from SQL, merge data, Raw v.s processed data
+
+##Exploratory data analysis content
+##Analytic Graphics, plotting, hierachical clustering, dimension reduction
+
+##reproducible research overview
+##structure of data analysis, markdown, latex, r markdown, rpubs
+
+##statistical inference content
+##fundamentla statistics
+
+##Regression model overview
+##Linear models, regression to the mean, multivariable regression analyses
+
+##Practical machine learning
+
+##Building Data Product Overview
+##R packages, r Charts, Slidify, Shiny
+##
